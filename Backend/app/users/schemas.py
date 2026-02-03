11ef82136b2134f5bd3ad26a756759b849e36e80
charles <cyphersmae@gmail.com>
2026-02-04 00:38:20 +0300

    added a schemas file users

--- Backend/app/users/schemas.py
+++ Backend/app/users/schemas.py
@@ -1,25 +1,25 @@
-# from pydantic import BaseModel, EmailStr
-# from uuid import UUID
-# 
-# 
-# class UserCreate(BaseModel):
-    # name: str
-    # email: EmailStr
-    # password: str
-    # role: str = "CLIENT"
-# 
-# 
-# class UserUpdate(BaseModel):
-    # name: str | None = None
-    # email: EmailStr | None = None
-    # role: str | None = None
-# 
-# 
-# class UserResponse(BaseModel):
-    # id: UUID
-    # name: str
-    # email: EmailStr
-    # role: str
-# 
-    # class Config:
-        # from_attributes = True
+from pydantic import BaseModel, EmailStr
+from uuid import UUID
+
+
+class UserCreate(BaseModel):
+    name: str
+    email: EmailStr
+    password: str
+    role: str = "CLIENT"
+
+
+class UserUpdate(BaseModel):
+    name: str | None = None
+    email: EmailStr | None = None
+    role: str | None = None
+
+
+class UserResponse(BaseModel):
+    id: UUID
+    name: str
+    email: EmailStr
+    role: str
+
+    class Config:
+        from_attributes = True
