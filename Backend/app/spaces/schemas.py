68838c02885cc2e55fa4396e600c92002ee0b4ab
charles <cyphersmae@gmail.com>
2026-02-03 23:46:47 +0300

    added a schemas file on space

--- Backend/app/spaces/schemas.py
+++ Backend/app/spaces/schemas.py
@@ -1,33 +1,33 @@
-# from pydantic import BaseModel
-# from uuid import UUID
-# from decimal import Decimal
-# 
-# 
-# class SpaceCreate(BaseModel):
-    # title: str
-    # description: str
-    # location: str
-    # price_per_hour: Decimal
-    # price_per_day: Decimal
-# 
-# 
-# class SpaceUpdate(BaseModel):
-    # title: str | None = None
-    # description: str | None = None
-    # location: str | None = None
-    # price_per_hour: Decimal | None = None
-    # price_per_day: Decimal | None = None
-    # status: str | None = None
-# 
-# 
-# class SpaceResponse(BaseModel):
-    # id: UUID
-    # title: str
-    # description: str
-    # location: str
-    # price_per_hour: Decimal
-    # price_per_day: Decimal
-    # status: str
-# 
-    # class Config:
-        # from_attributes = True
+from pydantic import BaseModel
+from uuid import UUID
+from decimal import Decimal
+
+
+class SpaceCreate(BaseModel):
+    title: str
+    description: str
+    location: str
+    price_per_hour: Decimal
+    price_per_day: Decimal
+
+
+class SpaceUpdate(BaseModel):
+    title: str | None = None
+    description: str | None = None
+    location: str | None = None
+    price_per_hour: Decimal | None = None
+    price_per_day: Decimal | None = None
+    status: str | None = None
+
+
+class SpaceResponse(BaseModel):
+    id: UUID
+    title: str
+    description: str
+    location: str
+    price_per_hour: Decimal
+    price_per_day: Decimal
+    status: str
+
+    class Config:
+        from_attributes = True
